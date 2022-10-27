efc571b7b83d23833eded915548f492bcb25ebb2
fialkovod <info@fialkovod.ru>
2022-10-27 16:42:10 +0300

    bug with unique quadruplet

--- 18. 4Sum/index.js
+++ 18. 4Sum/index.js
@@ -4,5 +4,29 @@
  * @return {number[][]}
  */
  var fourSum = function(nums, target) {
-    
+    let res=[], left, right, sum;
+    if(nums.length<4) return [];
+    nums.sort((n,m)=>n-m); 
+    for(let i=0; i<=nums.length-4; i++) {
+        if(nums[i]>target) return res;
+        for(let j=i+1; j<=nums.length-3; j++) {     
+            if(nums[i]+nums[j]>target) break;
+            left=j+1; right=nums.length-1;
+            while(left<right) {
+                sum=nums[i]+nums[j]+nums[left]+nums[right];
+                if(sum===target) {
+                    res.push([nums[i],nums[j],nums[left],nums[right]]);
+                    left++;
+                    right--;
+                } else {
+                    if(sum>target) {
+                        right--;
+                    } else {
+                        left++;
+                    } 
+                }
+            }
+        }
+    } 
+    return res;     
 };
